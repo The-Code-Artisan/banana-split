4108203b5cf854b5aaf507ad57b1ea621f9f0cc0
Prajwal Raj <54879620+Prajwal9798@users.noreply.github.com>
2026-03-01 00:28:09 +0530

    creating tables

--- database/migrations/V001_create_database.sql
+++ database/migrations/V001_create_database.sql
@@ -1,8 +1,25 @@
--- Create a useres table
 
-CREATE TABLE useres(
+
+CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL primary_key,
     username TEXT,
     email_id TEXT,
     mobile_number DECIMAL(10,0),
+);
+
+CREATE TABLE IF NOT EXISTS expences(
+    transaction_id SERIAL primary_key,
+    transaction_name TEXT,
+    transaction_amount BIGINT,
+    users_spend JSONB
+);
+
+CREATE TABLE IF NOT EXISTS balances(
+    transaction_id INT,
+    lender_id INT REFERENCES users(user_id),
+    borrower INT REFERENCES users(user_id),
+    CONSTRAINT fk_transactions
+        FOREIGN KEY transaction_id
+        REFERENCES expences(transaction_id)
+        ON DELETE CASCADE
 );
