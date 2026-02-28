2ac9b290aede907db6fa4ae6d803cede04273fb8
Prajwal Raj <54879620+Prajwal9798@users.noreply.github.com>
2026-03-01 00:34:27 +0530

    error fix

--- database/migrations/V001_create_database.sql
+++ database/migrations/V001_create_database.sql
@@ -1,25 +1,19 @@
-
-
 CREATE TABLE IF NOT EXISTS users(
-    user_id SERIAL primary_key,
+    user_id SERIAL primary key,
     username TEXT,
     email_id TEXT,
-    mobile_number DECIMAL(10,0),
+    mobile_number DECIMAL(10,0)
 );
 
 CREATE TABLE IF NOT EXISTS expences(
-    transaction_id SERIAL primary_key,
+    transaction_id SERIAL primary key,
     transaction_name TEXT,
     transaction_amount BIGINT,
     users_spend JSONB
 );
 
 CREATE TABLE IF NOT EXISTS balances(
-    transaction_id INT,
+    transaction_id INT REFERENCES expences(transaction_id),
     lender_id INT REFERENCES users(user_id),
-    borrower INT REFERENCES users(user_id),
-    CONSTRAINT fk_transactions
-        FOREIGN KEY transaction_id
-        REFERENCES expences(transaction_id)
-        ON DELETE CASCADE
+    borrower INT REFERENCES users(user_id)
 );
