fec7a1e6446f35db5ca633f443d902d2a8e1def6
Prajjwal <prajjwaldimri@hotmail.com>
2026-03-01 14:11:29 +0530

    sets up sqlc and pgx

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/the-code-artisan/banana-split/server/internal/repository"
 )
 
 func main() {
+	// DB Init
+	dbPool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to DB: %v\n", err)
+	}
+	defer dbPool.Close()
+
+	repo := repository.New(dbPool)
+
+	users, _ := repo.FindAllUsers(context.Background())
+	log.Printf("Users: %v", users)
+
 	router := http.NewServeMux()
 	router.HandleFunc("GET /ping", ping)
 
+	// Server Init
 	server := http.Server{
 		Addr:    ":8001",
 		Handler: router,
 	}
 	log.Println("Starting server on port :8001")
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Panicf("Unable to start server. Err: %v", err)
 	}
